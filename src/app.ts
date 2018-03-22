199976a77877ce216ca056f4bad470073fa41a0d
Alexander Taran <a.taran@outlook.com>
2018-03-23 02:08:06 +0300

    get projects from firestore

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+import { firebase } from '@firebase/app'
+import '@firebase/firestore'
+
 export class App {
-  message = 'Hello World!';
+  db: any;
+  message = 'Hello OSS World!';
+  projectsLoaded = false;
+  projects = []
+
+  constructor() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAQ4tNDdvJvATeNjso5Y5sCQN7DzdWsLqc",
+      authDomain: "oss-karma.firebaseapp.com",
+      databaseURL: "https://oss-karma.firebaseio.com",
+      projectId: "oss-karma",
+      storageBucket: "oss-karma.appspot.com",
+      messagingSenderId: "160724122516"
+    })
+    this.db = firebase.app().firestore();
+  }
+
+  async activate() {
+    //  let db = firebase.app().firestore()
+
+    let data = await this.db.collection('projects').get()
+    data.docs.forEach(d => {
+      this.projects.push(d.data())
+    })
+    this.projectsLoaded=true;
+  }
 }
