14fb97e02cddafbdb4e01f862bd17460ebec0548
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-23 11:08:20 +1000

    test(app): fix the app test and deps

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -1,8 +1,14 @@
-import {App} from '../../src/app';
+import { Container } from 'aurelia-dependency-injection';
+import { App } from '../../src/app';
+import { Store } from 'aurelia-store';
 import { FirebaseService } from '../../src/services/firebase-service';
+
+const container = new Container();
+const firebaseService = container.get(FirebaseService);
+const store = container.get(Store);
 
 describe('the app', () => {
   it('says hello', () => {
-    expect(new App(new FirebaseService()).message).toBe('Hello World!');
+    expect(new App(firebaseService, store).message).toBe('Hello World!');
   });
 });
