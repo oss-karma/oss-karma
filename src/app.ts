8416c7a47141f00147e6930381469d764176c50e
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-23 10:09:41 +1000

    refact(app): remove firebase code from app and inject service

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,22 @@
-import { firebase } from '@firebase/app'
-import '@firebase/firestore'
+import { autoinject } from 'aurelia-dependency-injection';
+import { FirebaseService } from './services/firebase-service';
 
+@autoinject()
 export class App {
   db: any;
   message = 'Hello OSS World!';
   projectsLoaded = false;
   projects = []
 
-  constructor() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyAQ4tNDdvJvATeNjso5Y5sCQN7DzdWsLqc",
-      authDomain: "oss-karma.firebaseapp.com",
-      databaseURL: "https://oss-karma.firebaseio.com",
-      projectId: "oss-karma",
-      storageBucket: "oss-karma.appspot.com",
-      messagingSenderId: "160724122516"
-    })
-    this.db = firebase.app().firestore();
+  constructor(private firebaseService: FirebaseService) {
+
   }
 
   async activate() {
-    //  let db = firebase.app().firestore()
+    const data = await this.firebaseService.getCollection('projects');
 
-    let data = await this.db.collection('projects').get()
-    data.docs.forEach(d => {
-      this.projects.push(d.data())
-    })
-    this.projectsLoaded=true;
+    data.docs.forEach(d => this.projects.push(d.data()));
+
+    this.projectsLoaded = true;
   }
 }
