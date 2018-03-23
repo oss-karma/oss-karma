7e8617bb6d810384214d372de21ef6dc9bdfc8a9
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-23 11:07:33 +1000

    feat(app): default state, constants and actions for store

---
+++ src/store/actions.ts
@@ -0,0 +1,22 @@
+import { Container } from 'aurelia-dependency-injection';
+import { Store } from 'aurelia-store';
+import { State } from './state';
+
+import { FirebaseService } from '../services/firebase-service';
+
+const API: FirebaseService = Container.instance.get(FirebaseService);
+const STORE: Store<State> = Container.instance.get(Store);
+
+export async function setUser(state, user) {
+  return { ...state, ...{ user } };
+}
+
+export async function loadProjects(state) {
+  const LOADED_PROJECTS = await API.getCollection('projects');
+
+  let projects = [];
+
+  LOADED_PROJECTS.docs.forEach(d => projects.push(d.data()));
+
+  return { ...state, ...{ projects } };
+}
---
+++ src/store/constants.ts
@@ -0,0 +1,2 @@
+export const LOAD_PROJECTS = 'loadProjects';
+export const SET_USER = 'setUser';
